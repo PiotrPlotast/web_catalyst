c0622e1b42ebf828ae0fcb61d0c4c6221353d8b2
Pietrell <piotrplotast2006@gmail.com>
2024-05-26 18:46:04 +0200

    remove google signin

--- web_catalyst/src/LoginPage.jsx
+++ web_catalyst/src/LoginPage.jsx
@@ -138,25 +138,6 @@ export default function LoginPage() {
               Sign up
             </button>
           </form>
-          <div className="flex my-2 items-center gap-2">
-            <span className="h-[1px] bg-[#D9D9D9] w-full"></span>
-            <span className="text-[#D9D9D9] ">or</span>
-            <span className="h-[1px] bg-[#D9D9D9] w-full"></span>
-          </div>
-          <button className="bg-[#2D98DA] border border-[#fff] text-[#fff] py-2 rounded w-full flex items-center justify-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="icon icon-tabler icons-tabler-filled icon-tabler-brand-google"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 2a9.96 9.96 0 0 1 6.29 2.226a1 1 0 0 1 .04 1.52l-1.51 1.362a1 1 0 0 1 -1.265 .06a6 6 0 1 0 2.103 6.836l.001 -.004h-3.66a1 1 0 0 1 -.992 -.883l-.007 -.117v-2a1 1 0 0 1 1 -1h6.945a1 1 0 0 1 .994 .89c.04 .367 .061 .737 .061 1.11c0 5.523 -4.477 10 -10 10s-10 -4.477 -10 -10s4.477 -10 10 -10z" />
-            </svg>
-            Sign up with Google
-          </button>
           <p className="text-[#D9D9D9] text-center text-pretty text mt-2">
             By creating an account you agree with our{" "}
             <a href="#" className="text-[#fff] hover:underline">
